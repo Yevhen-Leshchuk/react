6e3f3696ed74cf56dc129a1f55408b6ed418eb44
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2022-02-17 22:32:55 +0200

    add react-04

---
+++ react-04/src/App.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import shortid from 'shortid';
+// import Counter from './components/Counter';
+// import Dropdown from './components/Dropdown';
+// import ColorPicker from './components/ColorPicker';
+import Container from './components/Container';
+import TodoList from './components/TodoList';
+import TodoEditor from './components/TodoEditor';
+import Filter from './components/Filter';
+import initialTodos from './todos.json';
+// import Form from './components/Form';
+import './index.css';
+
+/*
+// Рендер компонента 
+*/
+
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Component State</h1>
+
+//       {/* <Dropdown /> */}
+//       {/* <Counter initialValue={12} /> */}
+//       {/* <ColorPicker options={colorPickerOptions} /> */}
+//       <TodoList />
+//     </>
+//   );
+// };
+
+class App extends Component {
+  state = {
+    todos: initialTodos,
+    filter: '',
+  };
+
+  addTodo = text => {
+    // console.log(text);
+
+    const todo = {
+      id: shortid.generate(),
+      text,
+      completed: false,
+    };
+
+    this.setState(({ todos }) => ({ todos: [todo, ...todos] }));
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  toggleCompleted = todoId => {
+    // this.setState(prevState => ({
+    //   todos: prevState.todos.map(todo => {
+    //     if (todo.id === todoId) {
+    //       return {
+    //         ...todo,
+    //         completed: !todo.completed,
+    //       };
+    //     }
+
+    //     return todo;
+    //   }),
+    // }));
+
+    this.setState(({ todos }) => ({
+      todos: todos.map(todo =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({
+      filter: event.currentTarget.value,
+    });
+  };
+
+  getVisibleTodos = () => {
+    const { todos, filter } = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    return todos.filter(todo =>
+      todo.text.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  calculateCompletedTodos = () => {
+    const { todos } = this.state;
+
+    return todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0,
+    );
+  };
+
+  // handleNameChange = event => {
+  //   console.log(event.currentTarget.value);
+
+  //   this.setState({ name: event.currentTarget.value });
+  // };
+
+  // handleTagChange = event => {
+  //   console.log(event.currentTarget.value);
+
+  //   this.setState({ tag: event.currentTarget.value });
+  // };
+
+  // formSubmitHandler = data => {
+  //   console.log(data);
+  // };
+
+  render() {
+    const { todos, filter } = this.state;
+
+    const totalTodoCount = todos.length;
+    const completedTodoCount = this.calculateCompletedTodos();
+    const visibleTodos = this.getVisibleTodos();
+
+    return (
+      <Container>
+        <div className="TodoBox">
+          <p>Общее кол-во: {totalTodoCount}</p>
+          <p>Кол-во выполненных: {completedTodoCount}</p>
+        </div>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <TodoEditor onSubmit={this.addTodo} />
+        {/* <Form onSubmit={this.formSubmitHandler} /> */}
+        {/* <Dropdown /> */}
+        {/* <Counter initialValue={12} /> */}
+        {/* <ColorPicker options={colorPickerOptions} /> */}
+        <TodoList
+          todos={visibleTodos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
+      </Container>
+    );
+  }
+}
+
+export default App;
