cd2934b7e009006672e226b4090f2f0dca38d3ed
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2022-03-14 23:02:45 +0200

    add react-06

---
+++ react-06/src/components/PokemonInfo.js
@@ -0,0 +1,59 @@
+import { Component } from 'react';
+import PokemonDataView from './PokemonDataView';
+import PokemonErrorView from './PokemonErrorView';
+import PokemonPendingView from './PokemonPendingView';
+import pokemonAPI from '../services/pokemon-api';
+
+// const Status = {
+//   IDLE: 'idle',
+//   PENDING: 'pending',
+//   RESOLVED: 'resolved',
+//   REJECTED: 'rejected',
+// };
+
+export default class PokemonInfo extends Component {
+  state = {
+    pokemon: null,
+    error: null,
+    status: 'idle',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevProps.pokemonName;
+    const nextName = this.props.pokemonName;
+
+    if (prevName !== nextName) {
+      // console.log('Змінилося ім"я покімона');
+      // console.log('prevProps.pokemonName', prevProps.pokemonName);
+      // console.log('this.props.pokemonName', this.props.pokemonName);
+
+      this.setState({ status: 'pending' });
+
+      pokemonAPI
+        .fetchPokemon(nextName)
+        .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
+        .catch(error => this.setState({ error, status: 'rejected' }));
+    }
+  }
+
+  render() {
+    const { pokemon, error, status } = this.state;
+    const { pokemonName } = this.props;
+
+    if (status === 'idle') {
+      return <div>Enter a pokemon name</div>;
+    }
+
+    if (status === 'pending') {
+      return <PokemonPendingView pokemonName={pokemonName} />;
+    }
+
+    if (status === 'rejected') {
+      return <PokemonErrorView message={error.message} />;
+    }
+
+    if (status === 'resolved') {
+      return <PokemonDataView pokemon={pokemon} />;
+    }
+  }
+}
