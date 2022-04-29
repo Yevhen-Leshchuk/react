8d7f26c417bfde360ecdd0850c90ca632c3e5b5d
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2022-04-29 19:50:03 +0300

    add react-13.1

---
+++ react-13.1/src/redux/store.js
@@ -0,0 +1,32 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import todosReducer from './todos/todos-reducer';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+// const persistor = persistStore(store);
+
+export default store;
